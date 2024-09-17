3bd3106914c0ea94d9057b0c872b01c972662cd3
Nguyen Ngoc Lan <nnguyenngoclan@minu.ai>
2024-09-17 09:06:06 +0700

    Implement default logger

--- lib/logger.dart
+++ lib/logger.dart
@@ -1,3 +1,42 @@
-import 'package:logger/logger.dart';
 
-final logger = Logger();
+import 'package:flutter/foundation.dart';
+
+abstract class Logger {
+  void log<T>(T data);
+
+  void i<T>(T data);
+
+  void e<T>(T data);
+
+  const Logger();
+
+  factory Logger.create() {
+    return _DefaultLogger();
+  }
+}
+
+class _DefaultLogger extends Logger {
+  @override
+  void log<T>(T data) {
+    if (kDebugMode) {
+      print(data);
+    }
+  }
+  
+  @override
+  void i<T>(T data) {
+    if (kDebugMode) {
+      print("\x1B[34m$data\x1B[0m");
+    }
+  }
+  
+  @override
+  void e<T>(T data) {
+    if (kDebugMode) {
+      print("\x1B[$data\x1B[0m");
+    }
+  }
+  
+}
+
+final logger = Logger.create();
